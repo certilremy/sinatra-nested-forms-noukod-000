9c1785e34cc8db4ed6d31878c602efd2a3a191cd
Certil Remy <certil@noukod.com>
2019-01-11 17:44:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,21 +7,21 @@ module FormsLab
     get '/' do
       erb :root
     end
-    
+
     get '/new' do
       erb :'pirates/new'
     end
-    
+
     post '/pirates' do
       @pirate = Pirate.new params[:pirate]
-      
+
       params[:pirate][:ships].each do |ship|
         Ship.new ship
       end
-      
+
       @ship1 = Ship.all.first
       @ship2 = Ship.all[1]
-      
+
       erb :'pirates/show'
     end
   end
